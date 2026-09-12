643a6613041d0deb9f8684ce62eb818985b71574
GodefroyL <godefroy.lecluse@gmail.com>
2026-09-12 14:09:05 +0200

    test site js

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def get_sondages_second_tour(annee: str):
     """
     return {"message": f"Récupération des sondages du second tour n'est pas encore implémentée."}
 
+app.frontend("/accueil", directory="frontend")
+
 if __name__ == "__main__":
     sondages = get_sondages_premer_tour("2027")
     print(sondages)
